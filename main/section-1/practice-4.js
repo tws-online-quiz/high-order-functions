b09b6b1d3307e504bb5cf79d872d707567dcfebb
Seaborn Lee <seabornlee.cn@gmail.com>
2017-08-25 16:51:59 +0800

    Complete section1

--- main/section-1/practice-4.js
+++ main/section-1/practice-4.js
@@ -1,21 +1,5 @@
 'use strict';
 
-function includes(collection, ch) {
-    for (let item of collection) {
-        if (item === ch) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
 module.exports = function collectSameElements(collectionA, objectB) {
-    let result = [];
-    for (let item of collectionA) {
-        if (includes(objectB.value, item.key)) {
-            result.push(item.key);
-        }
-    }
-    return result;
+    return collectionA.filter(obj => objectB.value.includes(obj.key)).map(obj => obj.key);
 }
